0bc0ed62814a8c201215fca05837a0860a7853b8
Joe Contreras <joe.contreras888@gmail.com>
2022-03-24 15:35:01 -0600

    Posts work

--- src/Feed/Feed.tsx
+++ src/Feed/Feed.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Posts from "./Posts/Posts";
 import { Grid, Paper } from "@mui/material";
+import Stories from "../components/stories";
 
 const Feed = () => {
   return (
     <>
+      <Stories />
       <Paper
         sx={{ backgroundColor: "#FAFAFA", height: "100%", minHeight: "100vh" }}
       >
--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,18 +1,17 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom';
-import Navbar from './Navbar/Navbar';
-import Stories from './stories';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar/Navbar";
+import Feed from "../Feed/Feed";
 
-const Router = function() {
-    return(
-        <BrowserRouter>
-            <Navbar/>
-            <Stories/>
-            <Routes>
-                <Route path="/" element={<h2>Home</h2>} />
-                <Route path="/profile" element={<h2>Profile</h2>} />
-            </Routes>
-        </BrowserRouter>
-    );
-}
+const Router = function () {
+  return (
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Feed />} />
+        <Route path="/profile" element={<h2>Profile</h2>} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default Router;
