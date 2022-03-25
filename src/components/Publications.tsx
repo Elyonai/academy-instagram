cc24d72ee2fcc69d89e73127d9ce8546e9d3e243
Joe Contreras <joe.contreras888@gmail.com>
2022-03-25 11:24:26 -0600

    Profile done

--- src/components/Publications.tsx
+++ src/components/Publications.tsx
@@ -1,64 +1,62 @@
-import * as React from 'react';
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
-import '../scss/Publications.scss';
+import * as React from "react";
+import ImageList from "@mui/material/ImageList";
+import ImageListItem from "@mui/material/ImageListItem";
+import "../scss/Publications.scss";
 
-export default function Plubications() {
-    return (
-        <div className='publicaciones'>
-            <ImageList sx={{ width: 600, height: 600 }} cols={3} rowHeight={50}>
-                {itemData.map((item) => (
-
-                    <ImageListItem key={item.img}>
-                        <img
-                            src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
-                            srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
-                            alt={item.title}
-                            loading="lazy"
-                        />
-                    </ImageListItem>
-
-                ))}
-            </ImageList>
-        </div>
-    );
+export default function Publications() {
+  return (
+    <div className="publicaciones">
+      <ImageList sx={{ width: 600, height: 600 }} cols={3} rowHeight={50}>
+        {itemData.map((item) => (
+          <ImageListItem key={item.img}>
+            <img
+              src={`${item.img}?w=164&h=164&fit=crop&auto=format`}
+              srcSet={`${item.img}?w=164&h=164&fit=crop&auto=format&dpr=2 2x`}
+              alt={item.title}
+              loading="lazy"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
+    </div>
+  );
 }
 
 const itemData = [
-    {
-        img: 'https://images.pexels.com/photos/245035/pexels-photo-245035.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-        title: 'Husky',
-    },
-    {
-        img: 'https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-        title: 'Canada',
-    },
-    {
-        img: 'https://images.pexels.com/photos/3359734/pexels-photo-3359734.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-        title: 'HP',
-    },
-    {
-        img: 'https://images.pexels.com/photos/2538225/pexels-photo-2538225.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-        title: 'Beach',
-    },
-    {
-        img: 'https://images.pexels.com/photos/214574/pexels-photo-214574.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-        title: 'Happy',
-    },
-    {
-        img: 'https://images.pexels.com/photos/3747505/pexels-photo-3747505.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-        title: 'school',
-    },
-    {
-        img: 'https://images.pexels.com/photos/414630/pexels-photo-414630.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-        title: 'working',
-    },
-    {
-        img: 'https://images.pexels.com/photos/1264210/pexels-photo-1264210.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-        title: 'photographer',
-    },
-    {
-        img: 'https://images.pexels.com/photos/4009402/pexels-photo-4009402.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-        title: 'couple',
-    },
+  {
+    img: "https://images.pexels.com/photos/245035/pexels-photo-245035.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "Husky",
+  },
+  {
+    img: "https://images.pexels.com/photos/417074/pexels-photo-417074.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "Canada",
+  },
+  {
+    img: "https://images.pexels.com/photos/3359734/pexels-photo-3359734.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "HP",
+  },
+  {
+    img: "https://images.pexels.com/photos/2538225/pexels-photo-2538225.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "Beach",
+  },
+  {
+    img: "https://images.pexels.com/photos/214574/pexels-photo-214574.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "Happy",
+  },
+  {
+    img: "https://images.pexels.com/photos/3747505/pexels-photo-3747505.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "school",
+  },
+  {
+    img: "https://images.pexels.com/photos/414630/pexels-photo-414630.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "working",
+  },
+  {
+    img: "https://images.pexels.com/photos/1264210/pexels-photo-1264210.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "photographer",
+  },
+  {
+    img: "https://images.pexels.com/photos/4009402/pexels-photo-4009402.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "couple",
+  },
 ];
--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,41 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './Navbar/Navbar';
-import Feed from '../Feed/Feed';
-import UserInfoComponent from '../components/user-info/user-info';
-import UserInfo from '../types/user-info';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Navbar/Navbar";
+import Feed from "../Feed/Feed";
+import Profile from "../Profile/Profile";
 
 const Router = function () {
-  const urlImage =
-    'https://img2.freepng.es/20180920/yko/kisspng-computer-icons-portable-network-graphics-avatar-ic-5ba3c66df14d32.3051789815374598219884.jpg';
-
-  const userInfo: UserInfo = {
-    name: 'Elyonai',
-    lastName: 'Hernandez',
-    userName: 'elyolml',
-    publications: 231,
-    followers: 134,
-    follows: 294,
-    profileImg: urlImage,
-  };
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Feed />} />
-        <Route
-          path='/profile'
-          element={
-            <UserInfoComponent
-              name={userInfo.name}
-              lastName={userInfo.lastName}
-              userName={userInfo.userName}
-              publications={userInfo.publications}
-              followers={userInfo.followers}
-              follows={userInfo.follows}
-              profileImg={userInfo.profileImg}
-            />
-          }
-        />
+        <Route path="/" element={<Feed />} />
+        <Route path="/profile" element={<Profile />} />
       </Routes>
     </BrowserRouter>
   );
