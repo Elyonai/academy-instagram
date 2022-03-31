6449b165bda3f9f35cedb23f6ba39b9c691035e2
diahnayala <99747487+diahnayala@users.noreply.github.com>
2022-03-31 13:06:43 -0600

    Storybook de botón login

---
+++ src/stories/Login.stories.js
@@ -0,0 +1,7 @@
+import Button from "react-bootstrap/Button";
+
+export const BotonPrincipal = () => (
+    <Button variant="info" style={{color:'white', fontWeight: 'bold'}}>
+        Iniciar sesión
+    </Button>
+)
