87951cbe9c184b34fa3097f3d08035d67de7651c
Mario B. Santana <bsantanamario@gmail.com>
2022-03-31 13:47:11 -0600

    stories fixed

--- src/components/stories.tsx
+++ src/components/stories.tsx
@@ -1,18 +1,17 @@
-/* eslint-disable jsx-a11y/alt-text */
 import { Component } from 'react';
 import Zuck from 'zuck.js';
 import '../styles/stories.scss';
 
 
 export default class Stories extends Component {
-  storiesElement = null;
+  storiesElement;
   state = {
     stories: [
       Zuck.buildTimelineItem(
-        'test1',
+        'test3',
         'https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/users/3.jpg',
-        'Test 3',
-        "https://ramon.codes",
+        'Test3',
+        '',
         1575221470504,
         [
           [
@@ -20,9 +19,7 @@ export default class Stories extends Component {
             'photo',
             3,
             'https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/6.jpg',
-            'https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/7.jpg',
             '',
-            false,
             false,
             1575221470504,
           ],
@@ -31,18 +28,17 @@ export default class Stories extends Component {
 
       Zuck.buildTimelineItem(
         'test2',
-        'https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/users/4.jpg',
-        'Test 2',
-        "https://ramon.codes",
+        'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTmfto5fYjQKvSo5VDiiFQ3I13Y7bOY2XCFbw&usqp=CAU',
+        'Test2',
+        '',
         1575221470504,
         [
           [
-            'test-1',
+            'test-2',
             'photo',
             3,
-            'https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/8.jpg',
+            'https://www.rockaxis.com/img/newsList/In%20Absentia%201.jpg',
             '',
-            false,
             false,
             1575221470504,
           ],
@@ -50,17 +46,16 @@ export default class Stories extends Component {
       ),
 
       Zuck.buildTimelineItem(
-        'test3',
+        'test1',
         'https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/users/2.jpg',
-        'Test 1',
-        "https://ramon.codes",
+        'Test1',
+        '',
         1575221470504,
         [
           [
-            'test-1',
+            'test-3',
             'photo',
             3,
-            'https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/5.jpg',
             'https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/5.jpg',
             '',
             false,
@@ -68,34 +63,23 @@ export default class Stories extends Component {
             1575221470504,
           ],
           [
-            'test-2',
+            'test-4',
             'video',
             0,
             'https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/4.mp4',
-            'https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/4.jpg',
+            false,
+            false,
             '',
-            false,
-            false,
-            1575221470504,
-          ],
-          [
-            'test-3',
-            'photo',
-            3,
-
-            'https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/1.jpg',
-            'https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/1.jpg',
-            false,
             1575221470504,
           ],
         ]
       ),
 
       Zuck.buildTimelineItem(
-        'Test 4',
-        'https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/users/3.jpg',
-        'Test 4',
-        'https://ramon.codes',
+        'test4',
+        'https://laverdadnoticias.com/export/sites/laverdad/img/2020/02/18/2_creepypastas_el_origen_de_rojo_vaca_y_el_pollito.png_1267727159.png',
+        'Test4',
+        '',
         1575221470504,
         [
           [
@@ -124,7 +108,6 @@ export default class Stories extends Component {
             'test-3',
             'photo',
             3,
-
             'https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/1.jpg',
             'https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/1.jpg',
             false,
@@ -133,46 +116,40 @@ export default class Stories extends Component {
         ]
       ),
       Zuck.buildTimelineItem(
-        'test 5',
-        'https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/users/4.jpg',
-        'Test 5',
-        'https://ramon.codes',
-        1575221470504,
+        'test5',
+        'https://nerdist.com/wp-content/uploads/2021/05/HimPowerPuff4.jpeg',
+        'Test5',
+        '',
+        1575221470505,
         [
           [
             'test-1',
             'photo',
-            3,
-            'https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/1.jpg',
-            'https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/1.jpg',
-            '',
+            0,
+            'https://www.looper.com/img/gallery/the-best-powerpuff-girls-episodes-according-to-imdb/power-noia-season-4-episode-12-1616431648.jpg',
             false,
-            false,
-            1575221470504,
           ],
           [
             'test-2',
-            'video',
-            0,
-            'https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/2.mp4',
-            'https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/2.jpg',
+            'photo',
+            1,
+            'https://i.stack.imgur.com/AcaRm.jpg',
             '',
             false,
-            false,
-            1575221470504,
           ],
           [
             'test-3',
             'photo',
-            3,
+            2,
 
-            'https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/1.jpg',
-            'https://raw.githubusercontent.com/ramon82/assets/master/zuck.js/stories/1.jpg',
+            'https://c.tenor.com/7JyiwIuB2jMAAAAC/the-powerpuff-girls-him.gif',
             false,
-            1575221470504,
+            '/',
           ],
         ]
       ),
+
+      
 
     ],
   };
@@ -194,49 +171,24 @@ export default class Stories extends Component {
       localStorage: false, // set true to save "seen" position. Element must have a id to save properly.
       reactive: true, // set true if you use frameworks like React to control the timeline (see react.sample.html)
       callbacks: {
-        onDataUpdate: function (currentState, callback) {
+        onDataUpdate:  (currentState: any, callback: () => void) => {
           this.setState(
-            (state) => {
-              state.stories = currentState;
-
-              return state;
-            },
-            () => {
+            
+            function () {
               callback();
             }
           );
-        }.bind(this),
+        },
       },
       stories: this.state.stories,
-      
     });
   }
 
   render() {
     const timelineItems = [];
 
-    this.state.stories.forEach((story, storyId) => {
+    this.state.stories.forEach((story, storyId): void => {
       const storyItems = [];
-
-      story.items.forEach((storyItem) => {
-        storyItems.push(
-          <li
-            key={storyItem.id}
-            data-id={storyItem.id}
-            data-time={storyItem.time}
-            className={storyItem.seen ? 'seen' : ''}
-          >
-            <a
-              href={storyItem.src}
-              data-type={storyItem.type}
-              data-length={storyItem.length}
-              data-link={storyItem.link}
-              data-linkText={storyItem.linkText}
-            >
-            </a>
-          </li>
-        );
-      });
 
       let arrayFunc = story.seen ? 'push' : 'unshift';
       timelineItems[arrayFunc](
@@ -255,7 +207,6 @@ export default class Stories extends Component {
               className='info'
               itemProp='author'
               itemScope={false}
-              itemType='http://schema.org/Person'
             >
               <strong className='name' itemProp='name'>
                 {story.name}
@@ -272,7 +223,7 @@ export default class Stories extends Component {
     return (
       <div>
         <div
-          ref={(node) => (this.storiesElement = node)}
+          ref={(node) => (this.storiesElement = node)}  
           id='stories-react'
           className='storiesWrapper'
         >
@@ -282,4 +233,3 @@ export default class Stories extends Component {
     );
   }
 }
-
